1dd5b3ba7b8e41d32a79820c0ac5e8b72404aa54
randypalusz <rpalusz@ncsu.edu>
2019-09-04 18:43:37 -0400

    added Problem 10 solution

---
+++ src/project/euler/Problem_10.java
@@ -0,0 +1,28 @@
+package project.euler;
+
+public class Problem_10 {
+    public static void main(String[] args){
+        //Account for 2 being a prime number
+        Double sum = 2.0;
+
+        for(double i = 3.0; i <= 2000000d; i++){
+            if(isPrime(i)){
+                sum = sum + i;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+
+    private static boolean isPrime(double num){
+        boolean retValue = true;
+        for(long i = (long) Math.sqrt(num); i>1; i--){
+            if(num%i == 0){
+                retValue = false;
+                break;
+            }
+        }
+        return retValue;
+    }
+}
