a7483060a85ccae0e65562e41b7fee74015f1dea
randypalusz <ibanezguy998@gmail.com>
2020-02-03 05:02:28 -0500

    Completed Problem 12

--- src/project/euler/Problem_12.java
+++ src/project/euler/Problem_12.java
@@ -1,5 +1,39 @@
 package project.euler;
 
 public class Problem_12 {
-    //USERNAME CHANGE BEGIN
+    public static void main(String[] args){
+        long triangleNum = 1;
+        long iterNum = 2;
+        do{
+            triangleNum = triangleNum + iterNum;
+            iterNum++;
+        }
+        while(!getDivisors(triangleNum));
+        System.out.println(triangleNum);
+    }
+
+    private static boolean getDivisors(long num){
+        int numDivisors = 0;
+        boolean meetsNod = false;
+        int sqrt = (int) Math.sqrt(num);
+
+        if(sqrt*sqrt == num){
+            numDivisors--;
+        }
+
+        for(int i = 1; i<=sqrt; i++){
+            if((num%i) == 0){
+                numDivisors = numDivisors + 2;
+            }
+            if(numDivisors > 500){
+                meetsNod = true;
+                break;
+            }
+        }
+
+        return meetsNod;
+    }
+
 }
+
+
