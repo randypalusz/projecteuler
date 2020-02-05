eb31d046e9e17e0dfc08d4b65c4508ef166cebc3
randypalusz <ibanezguy998@gmail.com>
2020-02-05 17:32:55 -0500

    Completed Problem 15

---
+++ src/project/euler/Problem_15.java
@@ -0,0 +1,21 @@
+package project.euler;
+
+import java.lang.Math;
+import java.math.BigInteger;
+
+public class Problem_15 {
+    public static void main(String[] args){
+        int d = 20;
+        BigInteger ans = nCr(2*d, d);
+        System.out.println(ans);
+    }
+
+    private static BigInteger nCr(final int n, final int r) {
+        BigInteger ret = BigInteger.ONE;
+        for (int i = 0; i < r; i++) {
+            ret = ret.multiply(BigInteger.valueOf(n-i))
+                    .divide(BigInteger.valueOf(i+1));
+        }
+        return ret;
+    }
+}
